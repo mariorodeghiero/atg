b2f7bd7847e188b0461092c7ff312deec477658b
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2021-02-05 12:50:17 -0300

    [Input][s]: add input logic

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,8 +1,30 @@
-import React from 'react'
-import * as S from "./styles"
+import React, { useState } from "react";
+import * as S from "./styles";
 
-const Input = props => {
-    return <S.Input />
-}
+const Input = () => {
+  const [value, setValue] = useState("");
+  const onFetchGame = () => {
+    fetch(
+      `https://www.atg.se/services/racinginfo/v1/api/products/${value.toUpperCase()}`
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+  };
 
-export default Input
+  const handleKeyPress = (event) => {
+    if (event.keyCode === 13) {
+      event.target.blur(event);
+    }
+  };
+  
+  return (
+    <S.Input
+      onChange={(event) => setValue(event.target.value)}
+      onKeyDown={(event) => handleKeyPress(event)}
+      type="text"
+      onBlur={onFetchGame}
+    />
+  );
+};
+
+export default Input;
