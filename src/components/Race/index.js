56dc7ffaec24ba2b6ef4d3dbfc385bac48e21464
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2021-02-05 12:51:23 -0300

    [RaceSection][s]: add fetch test logic

--- src/components/Race/index.js
+++ src/components/Race/index.js
@@ -3,8 +3,14 @@ import * as S from "./styles";
 
 const Race = ({ id, startTime }) => {
 
+  const onFetchRace = (id) => {
+    fetch(`https://www.atg.se/services/racinginfo/v1/api/games/${id}`)
+    .then(response => response.json())
+    .then(response => console.log(response))
+  }
+
   return (
-    <li onClick={() => console.log("test")}>
+    <li onClick={() => onFetchRace(id)}>
       {id} - {startTime}
     </li>
   );
