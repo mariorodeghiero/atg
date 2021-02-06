69dce8be983e2b0541db00d7d307814ce71dcfdb
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2021-02-06 11:11:12 -0300

    [index.js][s]: add provider + store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './pages/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import App from "./pages/App";
+import reportWebVitals from "./reportWebVitals";
+import { store, persistedStore } from "./store/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistedStore}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
