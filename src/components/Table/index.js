c1b6921a92c0aeb1a54696a3df5ab0054036beeb
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2021-02-06 21:24:42 -0300

    [refactor][s]: code refactor

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -87,24 +87,6 @@ function Row(props) {
   );
 }
 
-Row.propTypes = {
-  row: PropTypes.shape({
-    calories: PropTypes.number.isRequired,
-    carbs: PropTypes.number.isRequired,
-    fat: PropTypes.number.isRequired,
-    history: PropTypes.arrayOf(
-      PropTypes.shape({
-        amount: PropTypes.number.isRequired,
-        customerId: PropTypes.string.isRequired,
-        date: PropTypes.string.isRequired,
-      }),
-    ).isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    protein: PropTypes.number.isRequired,
-  }).isRequired,
-};
-
 export default function CollapsibleTable({races}) {
   return (
     <TableContainer component={Paper}>
@@ -129,14 +111,3 @@ export default function CollapsibleTable({races}) {
     </TableContainer>
   );
 }
-
-// User interface should contain input field to put game type in. Below the input field there should be
-// the area where information about the game is presented.
-// Information about the game should contain following data: - Game type - Races information (see
-// below)
-// For each race: - Race number - Race name - Race start time - Starts information (see below)
-// For each start: - Start number - Horse name - Driver/rider first name + last name - Trainer first
-// name + last name (expanded view) - Name of the horse father (expanded view)
-// Information about start should be presented in expandable block (collapsed by default). The
-// following information should be available in the expanded view: - Trainer first name + last name -
-// Name of the horse father
--- src/pages/App.js
+++ src/pages/App.js
@@ -6,20 +6,13 @@ import SectionRace from '../components/SectionRace';
 import GlobalStyle from '../styles/global.styles'
 
 const App = () => {
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    fetch("https://www.atg.se/services/racinginfo/v1/api/products/V75")
-      .then(response => response.json())
-      .then(response => setData(response.results))
-  },[])
 
   return (
     <>
       <GlobalStyle/>
       <Header title="Challenge"/>
       <Input/>
-      <SectionRace races={data}/>
+      <SectionRace/>
       <SectionGameInformation/>
     </>
   );
