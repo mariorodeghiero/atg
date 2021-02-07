e78dddb9aeaf0c2797fa0966049aec86dc46b985
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2021-02-07 13:54:07 -0300

    [SectionResults][xs]: fix margin-top

--- src/components/SectionResults/styles.js
+++ src/components/SectionResults/styles.js
@@ -3,12 +3,13 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
     width: 100%;
     max-width: 960px;
-    margin: 7rem auto 2rem auto;
+    margin: 8rem auto 2rem auto;
 `
 
 export const SubTitle = styled.h2`
     font-size: 1.5rem;
     color: var(--gray);
+    margin-bottom: 2rem;
 `
 
 export const List = styled.div`
