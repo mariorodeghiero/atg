b25d601c225b4925017da0d2b53719aa97065c8f
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2021-02-06 22:16:52 -0300

    [style][xs]: add root variables

--- src/styles/global.styles.js
+++ src/styles/global.styles.js
@@ -17,6 +17,13 @@ const GlobalStyles = createGlobalStyle`
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
+  :root{
+    --blue: #2A4F9F;
+    --white: #ffffff;
+    --yellow: #F7DB4A;
+    --gray-light: #f2f2f2;
+    --border-gray-light: rgba(242, 242, 242, 0.3);
+  }
 `
 
 export default GlobalStyles
